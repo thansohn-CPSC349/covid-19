bfda5140822b800b67b3862bf6b9acf78213a3c7
thansohn-CPSC349 <71525160+thansohn-CPSC349@users.noreply.github.com>
2020-12-20 16:17:44 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,57 +26,57 @@ function App() {
   let recovered = data.recovered ? data.recovered.toLocaleString(): "";
   let critical = data.critical ? data.critical.toLocaleString(): "";
   let percent_survived = ((data.recovered / data.cases) * 100).toFixed(1) + "%";
-  //let projected = `${(data.cases * 10).toLocaleString()} - ${(data.deaths * 1000).toLocaleString()}`
 
-  return (<>
-        <AppBar position="static" style={{ background: '#4d4d4d' }}>
-          <Toolbar>
-            <Typography variant="h6" style={{flexGrow: 1}}>
-              COVID-19 Data for United States - {new Date().toLocaleDateString()}
-            </Typography>
-          </Toolbar>
-          </AppBar><br/>
+  return (
+    <>
+      <AppBar position="static" style={{ background: '#4d4d4d' }}>
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            COVID-19 Data for United States - {new Date().toLocaleDateString()}
+          </Typography>
+        </Toolbar>
+        </AppBar><br/>
       <div className="info-container" align="center">
-          <div className="info-div1">
-            <SmallInfoWidget 
-            topText={cases}
-            topColor="#FF8C00"
-            bottomText="Total Confirmed COVID-19 Cases in the USA"
-            bottomColor="#000000"
-            />
-          </div>    
-          <div className="info-div2">
-            <SmallInfoWidget 
-            topText={deaths}
-            topColor="#B22222"
-            bottomText="Total Confirmed COVID-19 Deaths in the USA"
-            bottomColor="#000000"
-            />
-          </div>
-          <div className="info-div3">
-            <SmallInfoWidget 
-            topText={recovered}
-            topColor="#6495ED"
-            bottomText="Total COVID-19 Victims Who Recovered"
-            bottomColor="#000000"
-            />
-          </div> 
-          <div className="info-div4">
-            <SmallInfoWidget 
-            topText={critical}
-            topColor="#FF4500"
-            bottomText="Total COVID-19 Victims In Critical Condition"
-            bottomColor="#000000"
-            />
-          </div> 
-          <div className="info-div5">
-            <SmallInfoWidget 
-            topText={percent_survived}
-            topColor="#32CD32"
-            bottomText="Estimated US Recovery Rate"
-            bottomColor="#000000"
-            />
-          </div> 
+        <div className="info-div1">
+          <SmallInfoWidget 
+          topText={cases}
+          topColor="#FF8C00"
+          bottomText="Total Confirmed COVID-19 Cases in the USA"
+          bottomColor="#000000"
+          />
+        </div>    
+        <div className="info-div2">
+          <SmallInfoWidget 
+          topText={deaths}
+          topColor="#B22222"
+          bottomText="Total Confirmed COVID-19 Deaths in the USA"
+          bottomColor="#000000"
+          />
+        </div>
+        <div className="info-div3">
+          <SmallInfoWidget 
+          topText={recovered}
+          topColor="#6495ED"
+          bottomText="Total COVID-19 Victims Who Recovered"
+          bottomColor="#000000"
+          />
+        </div> 
+        <div className="info-div4">
+          <SmallInfoWidget 
+          topText={critical}
+          topColor="#FF4500"
+          bottomText="Total COVID-19 Victims In Critical Condition"
+          bottomColor="#000000"
+          />
+        </div> 
+        <div className="info-div5">
+          <SmallInfoWidget 
+          topText={percent_survived}
+          topColor="#32CD32"
+          bottomText="Estimated US Recovery Rate"
+          bottomColor="#000000"
+          />
+        </div> 
       </div><br/>
       <div className="map-container">
         <div className="map-div">
@@ -93,7 +93,7 @@ function App() {
             - By Tyler Hansohn - Data source used: <a href="http://disease.sh/">disease.sh</a> - Libraries used: bizCharts, d3, material-table, Material-UI, React.JS, react-simple-maps, react-tooltip - Data may take a few seconds to load -
         </div>
       </div>
-    </>
+   </>
   );
 }
 
